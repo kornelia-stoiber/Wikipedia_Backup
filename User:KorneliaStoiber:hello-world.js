721375408b64ef162de1ac4ff53d4a84173c65be
Kornelia Stoiber <17234998+kornelia-stoiber@users.noreply.github.com>
2024-10-13 11:07:26 +0200

    Create User:KorneliaStoiber:hello-world.js
    created file

---
+++ User:KorneliaStoiber:hello-world.js
@@ -0,0 +1,10 @@
+//$('#bodyContent').prepend('<h1>Hello world!</h1>');
+
+//window.alert(5 + 6);
+
+/*const now = new Date();
+const day = now.getDay(); // returns a number representing the day of the week, starting with 0 for Sunday
+const hours = now.getHours();
+const minutes = now.getMinutes();
+
+window.alert(`Today is day ${day} and the time is ${hours}:${minutes}.`);*/
